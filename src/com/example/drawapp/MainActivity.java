86914399ddf8a661841b599c03e6c55d7396d3c6
zshaheen <zeshawns@sbcglobal.net>
2014-07-23 23:54:45 -0700

    Save functionality expanded

--- src/com/example/drawapp/MainActivity.java
+++ src/com/example/drawapp/MainActivity.java
@@ -9,14 +9,21 @@ import java.io.OutputStream;
 
 import android.app.Activity;
 import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Bitmap.CompressFormat;
 import android.os.Bundle;
 import android.os.Environment;
+import android.text.Editable;
+import android.text.InputType;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.os.Build;
 
@@ -24,7 +31,7 @@ public class MainActivity extends Activity {
 	
 	private DrawingView drawView;
 	String dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/SnapDrawShare/";
-    
+    String filename = "temp";
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,29 +45,84 @@ public class MainActivity extends Activity {
     
     private void saveFile() throws FileNotFoundException {
     	
-    	String filename = "thing.png";
-    	OutputStream stream = new FileOutputStream(dir + filename);
-    	drawView.canvasBitmap.compress(CompressFormat.PNG, 100, stream);
+    	final String savePath = dir + filename+".png";
+    	File file = new File(savePath);
+    	if(file.exists()) {
+    		AlertDialog.Builder nameCollision = new AlertDialog.Builder(this);
+    		nameCollision.setTitle("Uh oh");
+    		TextView text = new TextView(this);
+    		text.setText("A file with the same name exists. Do you want to replace the existing file?");
+    		text.setGravity(Gravity.CENTER_HORIZONTAL);
+    		nameCollision.setView(text);
+    		nameCollision.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+    	        public void onClick(DialogInterface dialog, int whichButton) {
+    	        	OutputStream stream = null;
+					try {
+						stream = new FileOutputStream(savePath);
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						Toast.makeText(getApplicationContext(), "Error creating file stream", Toast.LENGTH_LONG).show();
+						e.printStackTrace();
+					}
+    		    	drawView.canvasBitmap.compress(CompressFormat.PNG, 100, stream);
+    		    	Toast.makeText(getApplicationContext(), "Image saved in Pictures/SnapDrawShare", Toast.LENGTH_LONG).show();
+    	        }
+    	        
+    	    });
+    		nameCollision.setNegativeButton("No", new DialogInterface.OnClickListener() {
+    	        public void onClick(DialogInterface dialog, int whichButton) {
+    	        	dialog.cancel();
+    	        }});
+    		
+    		nameCollision.show();
+    	}
     	
+    	else {
+    		OutputStream stream = null;
+			try {
+				stream = new FileOutputStream(savePath);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				Toast.makeText(getApplicationContext(), "Error creating file stream", Toast.LENGTH_LONG).show();
+				e.printStackTrace();
+			}
+	    	drawView.canvasBitmap.compress(CompressFormat.PNG, 100, stream);
+	    	Toast.makeText(getApplicationContext(), "Image saved in Pictures/SnapDrawShare", Toast.LENGTH_LONG).show();
+    	}
     }
     
     public void onSaveButton()  {
-    	/*File image;
-    	try {
-	    	image = createImageFile();
-    	} catch (IOException ex) {
-    		Toast.makeText(getApplicationContext(), "Error saving picture from camera", Toast.LENGTH_LONG).show();
-    		
-    	}*/
-    	try {
-			saveFile();
-		} catch (FileNotFoundException e) {
-			Toast.makeText(getApplicationContext(), "Error saving image", Toast.LENGTH_LONG).show();
-			e.printStackTrace();
-		}
+    	//open a dialog that allows the user to choose a name
+    	final EditText input = new EditText(this);
+    	input.setInputType(InputType.TYPE_CLASS_TEXT);
+
+    	
+    	AlertDialog.Builder saveDialog = new AlertDialog.Builder(this);
+		saveDialog.setTitle("Save Drawing");
+		saveDialog.setView(input);
+		saveDialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+	        public void onClick(DialogInterface dialog, int whichButton) {
+	        	filename = input.getText().toString();
+	        	
+	        	
+	        	try {
+	    			saveFile();
+	    		} catch (FileNotFoundException e) {
+	    			Toast.makeText(getApplicationContext(), "Error saving image", Toast.LENGTH_LONG).show();
+	    			e.printStackTrace();
+	    		}
+	        }
+	        
+   		});
+		saveDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+	        public void onClick(DialogInterface dialog, int whichButton) {
+	        	dialog.cancel();
+        }});
+		saveDialog.show();
+		
     	
     	
-    	Toast.makeText(getApplicationContext(), "Image saved in Pictures/SnapDrawShare", Toast.LENGTH_LONG).show();
+    	
     }
     
 
