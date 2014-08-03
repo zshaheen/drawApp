f79d1f189827c4a5213f2b6b63cb52795e16bdf9
zshaheen <zeshawns@sbcglobal.net>
2014-08-03 00:54:45 -0700

    Trim bitmap so pic isnt squashed
    works for only portrait pics

--- src/com/example/drawapp/DrawingView.java
+++ src/com/example/drawapp/DrawingView.java
@@ -2,11 +2,12 @@ package com.example.drawapp;
 
 import android.content.Context;
 import android.view.View;
+import android.os.Environment;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PorterDuff;
@@ -29,11 +30,13 @@ public class DrawingView extends View {
 	//canvas
 	private Canvas drawCanvas;
 	//canvas bitmap
-	public Bitmap canvasBitmap, tempBmp;
+	public Bitmap canvasBitmap, tempBmp, resizeBmp;
 	
 	private static float scale = 0; 
 	
 	public static boolean erase = false;
+	
+	private float xOffset=0, yOffset=0;
 	
 	
 	public DrawingView(Context context, AttributeSet attrs) {
@@ -50,24 +53,20 @@ public class DrawingView extends View {
 		
 		scale = getResources().getDisplayMetrics().density;
 		
-		//paintBrushSize varies from 1 to 300
-		//paintBrushSize = 20;
-		
 		drawPaint.setAntiAlias(true);
 		drawPaint.setStrokeWidth((float) (paintBrushSize * scale + 0.5f));
 		//TODO look into setStyle and setStrokeJoin
 		drawPaint.setStyle(Paint.Style.STROKE);
 		drawPaint.setStrokeJoin(Paint.Join.ROUND);
 		drawPaint.setStrokeCap(Paint.Cap.ROUND);
-		//drawPaint.setAlpha(0xFF);
-		//setLayerType(View.LAYER_TYPE_SOFTWARE, drawPaint);
+		
 		canvasPaint = new Paint(Paint.DITHER_FLAG);
-		//canvasBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
-		tempBmp = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
-		//canvasBitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
-		//drawCanvas = new Canvas(canvasBitmap.copy(Bitmap.Config.ARGB_8888, true));//(canvasBitmap.copy(Bitmap.Config.ARGB_8888, true));
 		
-		
+		//tempBmp = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
+		String dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/SnapDrawShare/";
+		BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+		tempBmp = BitmapFactory.decodeFile(dir+"pic.png",options);
 	}
 	
 	
@@ -108,26 +107,55 @@ public class DrawingView extends View {
 	@Override
 	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
 		super.onSizeChanged(w, h, oldw, oldh);
-		//canvasBitmap = Bitmap.createBitmap(tempBmp,12,12, w, h/*, Bitmap.Config.ARGB_8888*/);
+		
+		/* Uncropped, but leaves a whitespace
+		width = w;
+		Log.i("width", ""+width);
+		Log.i("pic_height", ""+tempBmp.getHeight());
+		Log.i("pic_width", ""+tempBmp.getWidth());
+		height = 1400;//(tempBmp.getHeight()/tempBmp.getWidth())*width;
+		Log.i("height", ""+height);
+		
+		//canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+		canvasBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+		drawCanvas = new Canvas(canvasBitmap);
+		resizeBmp = Bitmap.createScaledBitmap(tempBmp, width, height, false);
+		*/
+		float sourceW = tempBmp.getWidth(), sourceH = tempBmp.getHeight();
+		
 		canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
 		drawCanvas = new Canvas(canvasBitmap);
-		//canvasBitmap = Bitmap.createBitmap(tempBmp);
-		//drawCanvas = new Canvas(canvasBitmap);
-		//drawCanvas.drawColor(Color.WHITE);
-		//drawCanvas.drawColor(Color.TRANSPARENT);
 		
-		
+		if(sourceH >= sourceW) { 
+			//portrait 
+			int newWidth = (int) (h*(sourceW/sourceH));
+			resizeBmp = Bitmap.createScaledBitmap(tempBmp, newWidth, h, false);
+			//Now calculate how off the width (offsetX) should be
+			xOffset = -(newWidth - canvasBitmap.getWidth())/2;
+		}
+		else if(sourceW >= sourceH) {
+			//landscape
+			
+		}
+		Log.i("offsetX", ""+xOffset);
+		/* orig
+		centerCropBitmap(canvasBitmap, w, h);
+		resizeBmp = canvasBitmap;//Bitmap.createScaledBitmap(tempBmp, w, h, false);
+		centerCropBitmap(tempBmp, w, h);
+		resizeBmp = tempBmp;//Bitmap.createScaledBitmap(tempBmp, w, h, false);
+		*/
+	
 	}
 	
 	
 	@Override
 	protected void onDraw(Canvas canvas) {
-		canvas.drawBitmap(tempBmp, 0, 0, canvasPaint);
+		
+		canvas.drawBitmap(resizeBmp, xOffset, yOffset, canvasPaint);
 		canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
-		
-		
 		canvas.drawPath(drawPath, drawPaint);
 		setDrawingCacheEnabled(true);
+
 	}
 	
 	@Override
--- src/com/example/drawapp/MainActivity.java
+++ src/com/example/drawapp/MainActivity.java
@@ -48,18 +48,9 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       /*requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN);*/
-        /*WindowManager.LayoutParams attrs = getWindow().getAttributes();
-        attrs.flags |= WindowManager.LayoutParams.FLAG_FULLSCREEN;
-        getWindow().setAttributes(attrs);*/
         getWindow().setFlags(
         	    WindowManager.LayoutParams.FLAG_FULLSCREEN, 
         	    WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        //getWindow().getDecorView()
-        //.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-
 
         setContentView(R.layout.activity_main);
         imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
